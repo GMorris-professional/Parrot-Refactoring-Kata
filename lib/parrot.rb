647f64202465e5b0d5170f155ca4accd8473ed57
GMorris-Stream <gavinmo@vatitstream.com>
2023-03-03 13:08:15 +0200

    Step 4 - Abstraction

--- lib/parrot.rb
+++ lib/parrot.rb
@@ -9,26 +9,10 @@ class Parrot
 end
 
 class Type
-  class European < Type
+
+  class Base
     def initialize
-    end
 
-    def speed
-      return base_speed
-    end
-
-    private
-      def base_speed
-        12.0
-      end
-  end
-  class African < Type
-    def initialize(number_of_coconuts)
-      @number_of_coconuts = number_of_coconuts
-    end
-
-    def speed
-      return [0, base_speed - load_factor * @number_of_coconuts].max
     end
 
     private
@@ -40,7 +24,22 @@ class Type
         9.0
       end
   end
-  class NorwegianBlue < Type
+
+  class European < Base
+    def speed
+      return base_speed
+    end
+  end
+  class African < Base
+    def initialize(number_of_coconuts)
+      @number_of_coconuts = number_of_coconuts
+    end
+
+    def speed
+      return [0, base_speed - load_factor * @number_of_coconuts].max
+    end
+  end
+  class NorwegianBlue < Base
     def initialize(voltage, nailed)
       @voltage = voltage
       @nailed = nailed
@@ -49,11 +48,6 @@ class Type
     def speed
       return (@nailed) ? 0 : [24.0, @voltage * base_speed].min
     end
-
-    private
-      def base_speed
-        12.0
-      end
   end
 
   def self.for(type, number_of_coconuts, voltage, nailed)
